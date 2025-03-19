c90a48c58a2a73d4ae1896b86d44d4e8a8064627
anamilchert <ana.milchert@hotmail.com>
2025-03-19 11:12:56 -0300

    criando rotas

---
+++ backend/src/routes/userRoutes.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const User = require('../models/user');
+const router = express.Router();
+
+
+router.post('/users', async (req, res) => {
+  const { name, email, password, role } = req.body;
+  try {
+    const newUser = new User({ name, email, password, role });
+    await newUser.save();
+    res.status(201).json(newUser);
+  } catch (err) {
+    res.status(400).json({ message: 'Error creating user', error: err });
+  }
+});
+
+
+router.get('/users', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(400).json({ message: 'Error fetching users', error: err });
+  }
+});
+
+
+router.get('/users/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await User.findById(id);
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(400).json({ message: 'Error fetching user', error: err });
+  }
+});
+
+
+router.put('/users/:id', async (req, res) => {
+  const { id } = req.params;
+  const { name, email, password, role } = req.body;
+  try {
+    const user = await User.findByIdAndUpdate(id, { name, email, password, role }, { new: true });
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(400).json({ message: 'Error updating user', error: err });
+  }
+});
+
+
+router.delete('/users/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await User.findByIdAndDelete(id);
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    res.status(200).json({ message: 'User deleted' });
+  } catch (err) {
+    res.status(400).json({ message: 'Error deleting user', error: err });
+  }
+});
+
+module.exports = router;
