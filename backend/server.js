5f6015163fbcd1c986dfade6c044bac7ee1a48ad
anamilchert <ana.milchert@hotmail.com>
2025-03-19 11:18:44 -0300

    adicionando as rotas no servidor

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,21 @@
-const express = require("express");
+const express = require('express');
+const mongoose = require('mongoose');
+const userRoutes = require('./src/routes/userRoutes');
+const bookRoutes = require('./src/routes/bookRoutes');
+const loanRoutes = require('./src/routes/loanRoutes');
 
 const app = express();
-const PORT = 5000;
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Servidor está rodando!");
-});
+mongoose.connect('mongodb://localhost/biblioTech', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB conectado'))
+  .catch(err => console.log(err));
 
+app.use('/api', userRoutes);
+app.use('/api', bookRoutes);
+app.use('/api', loanRoutes);
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
